5c56bcb6ce80309bfe1fc957ea19bed573a41bec
Ali Bazzi <bazzi@bazman.dev>
2019-09-24 13:45:33 +0300

    deployment

--- app.js
+++ app.js
@@ -55,10 +55,10 @@ app.use("/graphql", bodyParser.json(), cookieParser(), (req, res) => {
 
 // Only run when in production
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('./frontend/build'));
 
   app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "./frontend", "build", "index.html"))
   })
 }
 
